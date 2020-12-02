b29da4bc9bdd6f87d8451da8f44092a1a7fef108
psystar99 <soyoung99.park@gmail.com>
2020-12-02 20:43:50 +0900

    okhttp 코드 정리- 확진자 정보 받기 성공

--- app/src/main/java/com/example/project/DBEntity.java
+++ app/src/main/java/com/example/project/DBEntity.java
@@ -21,31 +21,24 @@ import okhttp3.Response;
 public class DBEntity {
 
     /*DBEntity에서 계속 저장할 환자 리스트*/
-    private static ArrayList<Patient> patientList;
-    private static ArrayList<VisitPlace> dumVSP1;
-    private static ArrayList<VisitPlace> dumVSP2;
+    private static ArrayList<Patient> patientList=new ArrayList<Patient>();
 
     /*DB 연동시 필요한 필드들*/
-    private static DB task;
+
     private static String result = "success";//일단 임시 success
-    private String sendmsg;
 
-    /*AND메소드에서 임시로 쓸 필드들*/
-    private int size;
 
-    private static OkHttpClient client;
-    private static String OkhttpUrl;
-    private static Request request;
-    private static MediaType mediaType;
-    private static RequestBody body;
-    private static JSONArray patients;
-    private static JSONArray movingList;
+    private static OkHttpClient client=new OkHttpClient().newBuilder().build();
+    private static String OkhttpUrl="http://192.168.1.196:8080/WLP_re/androidDB.jsp";
+    private static MediaType mediaType=MediaType.parse("text/plain");
+
+    private static JSONArray patients=null;
+    private static JSONArray movingList=null;
 
 
     //patientList 값 접-----------------------------------------------------------------------------------------------
 
     public DBEntity() {
-        patientList = new ArrayList<Patient>();
 
     }
 
@@ -64,78 +57,10 @@ public class DBEntity {
     //Android 프로젝트에 저장되어 있는 patientlist 수정 메소드----------------------------------------------------------------------------------------------------
     // DB테이블 수정 메소드들의 반환값이 1로 선행되어야 함
 
-    /*관리자의 확진자 추가 페이지에서 확진자 정보를 추가하는 메소-동선 제외 */
-    public void AND_insert_patient(Patient patient) {
-        patientList.add(patient);
-    }
-
-    /*관리자의 확진자 추가 페이지에서 확진자 동선 정보 하나를 추가하는 메소드-무조건 해당 환자에 한 정보가 테이블에 저장되어 있어야 함. */
-    public static int AND_insert_pmoving(Patient patient, VisitPlace visitplace) {
-        for (int i = 0; i < patientList.size(); i++) {
-            Log.d("AND 환자 : ", patient.getPatientNum()+", "+Integer.toString(patient.getSmallLocalNum())+", "+Integer.toString(patient.getBigLocalNum()));
-            if (patient.getSmallLocalNum() == patientList.get(i).getSmallLocalNum() && patient.getBigLocalNum() == patientList.get(i).getBigLocalNum()
-                    && patient.getPatientNum().equals(patientList.get(i).getPatientNum())) {
-                //조건문 바꿔야 함! patient 테이블의 프라이머리 키는 지역번호랑 환자번호! 즉 patient와 patientList.get(i)의 정보가 같을 두객체가 같을 때——————————————————————————————————————————————————patient는 smalllocalnum,bigloculnum,patientnum 같으면 같은 객체!
-                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
-                patientList.get(i).getVisitPlaceList().add(visitplace);
-                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
-                //해당 환자에 visitplace 추가
-                return 1;// 동선 삽입 성공
-            }
-        }
-        return 0;//동선 삽입 실패: 해당 환자 정보가 없음
-    }
 
 
-    /*관리자의 확진자 수정 페이지에서 확진자 동선 정보 하나를 삭제하는 메소드 */
-    public static int AND_delete_pmoving(Patient patient, VisitPlace visitplace) {
-        //patient_info();
-        for (int i = 0; i < patientList.size(); i++) {
-            //1. 환자리스트를 돌며 동일한 환자 찾기.
-            //DB테이블에서의 primary 키값들의 값이 같을 때 동일 환자.
-            Log.d("AND 환자 : ", patient.getPatientNum()+", "+Integer.toString(patient.getSmallLocalNum())+", "+Integer.toString(patient.getBigLocalNum()));
-            if (patient.getSmallLocalNum() == patientList.get(i).getSmallLocalNum() && patient.getBigLocalNum() == patientList.get(i).getBigLocalNum()
-                    && patient.getPatientNum().equals(patientList.get(i).getPatientNum())){
-                //칮으면 해당 환자의 visitplacelist를 돌며 매개변수로 온 visitplace 찾고 삭제하기
-                int t = findIndex(patientList.get(i).getVisitPlaceList(),visitplace);
-                if(t!=-1){
-                    patientList.get(i).getVisitPlaceList().remove(t);
-                }
-                //setPatientList(patientList);
-                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
-                return 1;//정상 삭제
-            }
-        }
-        return -1;//환자 정보도 없을 경우
-    }
-    public static int findIndex(ArrayList<VisitPlace> visitplaceList,VisitPlace visitplace2){
-        for(int i =0; i <visitplaceList.size();i++){
-            if(visitplaceList.get(i).getVisitDate().equals(visitplace2.getVisitDate())&&
-                    visitplaceList.get(i).getVisitPlace().get_placeAddress().equals(visitplace2.getVisitPlace().get_placeAddress())){
-                return i;
-            }
-        }
-        return -1;
-    }
-    /*관리자의 확진자 추가, 수정 페이지에서 확진자 동선 정보 하나를 삭제하는 메소드-환자 정보를 삭제하면 관련 동선 정보도 싹다 삭제*/
-    public static void AND_delete_patient(Patient patient) {
-        patientList.remove(patient);
-    }
+
     public static void connectAppDB(){
-        client = new OkHttpClient().newBuilder().build();
-        mediaType = MediaType.parse("text/plain");
-        OkhttpUrl="http://3.35.210.3:8080/WLP_re/androidDB.jsp";
-
-        patientList = new ArrayList<Patient>();
-       // task = new DB("patients_info");
-        ArrayList<VisitPlace> temp = new ArrayList<VisitPlace>();
-        // int small=Integer.parseInt(plocnum.substring(0,1));
-        // int big=Integer.parseInt(plocnum.substring(2,3));
-        //박소영 놈이 제대로 DB연동하기 전까지 만든 임시 확진자 리스트—> 구축 후 삭제 예정
-
-        patients = null;
-        //JSONArray movingList=null;
-
         RequestBody body =new FormBody.Builder().add("type","patients_info").build();
         Request request = new Request.Builder().url(OkhttpUrl).method("POST", body).build();
 
@@ -154,27 +79,12 @@ public class DBEntity {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    //result =response.body().string();
-                    //Log.d("젼쥬", patients.toString());
-                    // Log.d("response jsp db",response.body().string()+"");
+                    Log.d("patient json array",patients.toString());
                 }
             }
         });
     }
     public static void connectMovingDB(){
-        client = new OkHttpClient().newBuilder().build();
-        mediaType = MediaType.parse("text/plain");
-        OkhttpUrl="http://3.35.210.3:8080/WLP_re/androidDB.jsp";
-
-        patientList = new ArrayList<Patient>();
-        task = new DB("patients_info");
-        ArrayList<VisitPlace> temp = new ArrayList<VisitPlace>();
-        // int small=Integer.parseInt(plocnum.substring(0,1));
-        // int big=Integer.parseInt(plocnum.substring(2,3));
-        //박소영 놈이 제대로 DB연동하기 전까지 만든 임시 확진자 리스트—> 구축 후 삭제 예정
-
-        patients = null;
-        //JSONArray movingList=null;
 
         RequestBody body =new FormBody.Builder().add("type","pmoving_info").build();
         Request request = new Request.Builder().url(OkhttpUrl).method("POST", body).build();
@@ -194,9 +104,7 @@ public class DBEntity {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    //result =response.body().string();
-                    //Log.d("젼쥬", patients.toString());
-                    Log.d("json array",movingList.toString());
+                    Log.d("pmoving json array",movingList.toString());
                 }
             }
         });
@@ -208,9 +116,6 @@ public class DBEntity {
     /*인트로 때, 앱 실행시 한 번만 불리는 메소드드*/
     public static ArrayList<Patient> patient_info() throws JSONException {
         ArrayList<VisitPlace> temp = new ArrayList<VisitPlace>();
-        // int small=Integer.parseInt(plocnum.substring(0,1));
-        // int big=Integer.parseInt(plocnum.substring(2,3));
-        //박소영 놈이 제대로 DB연동하기 전까지 만든 임시 확진자 리스트--> 구축 후 삭제 예정
 
         for(int i =0; i < patients.length();i++){
             JSONObject patient = patients.getJSONObject(i);
@@ -218,11 +123,13 @@ public class DBEntity {
             String plocnum = patient.getString("plocnum");
             String checkNum = plocnum+patient.getString("pnum");
             int small=Integer.parseInt(plocnum.substring(2));
-            Log.d("Small integer", plocnum.substring(2));
+            Log.d("Small integer", Integer.toString(small));
             int big=Integer.parseInt(plocnum.substring(0,2));
+
+
             for(int j =0; j < movingList.length();j++){
                 JSONObject moving = movingList.getJSONObject(j);
-                Log.d("json, size", Integer.toString(j));
+               // Log.d("json, size", Integer.toString(j));
                 String movingCheckNum = moving.getString("plocnum")+moving.getString("pnum");
                 if(checkNum.equals(movingCheckNum)){
                     temp.add(new VisitPlace(new Place(moving.getString("address"),
@@ -290,5 +197,63 @@ public class DBEntity {
         }catch (Exception e) {
             Log.i("DBtest", ".....ERROR.....!");
             return -2;}*/
+    }
+    /*관리자의 확진자 추가 페이지에서 확진자 정보를 추가하는 메소-동선 제외 */
+    public void AND_insert_patient(Patient patient) {
+        patientList.add(patient);
+    }
+
+    /*관리자의 확진자 추가 페이지에서 확진자 동선 정보 하나를 추가하는 메소드-무조건 해당 환자에 한 정보가 테이블에 저장되어 있어야 함. */
+    public static int AND_insert_pmoving(Patient patient, VisitPlace visitplace) {
+        for (int i = 0; i < patientList.size(); i++) {
+            Log.d("AND 환자 : ", patient.getPatientNum()+", "+Integer.toString(patient.getSmallLocalNum())+", "+Integer.toString(patient.getBigLocalNum()));
+            if (patient.getSmallLocalNum() == patientList.get(i).getSmallLocalNum() && patient.getBigLocalNum() == patientList.get(i).getBigLocalNum()
+                    && patient.getPatientNum().equals(patientList.get(i).getPatientNum())) {
+                //조건문 바꿔야 함! patient 테이블의 프라이머리 키는 지역번호랑 환자번호! 즉 patient와 patientList.get(i)의 정보가 같을 두객체가 같을 때——————————————————————————————————————————————————patient는 smalllocalnum,bigloculnum,patientnum 같으면 같은 객체!
+                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
+                patientList.get(i).getVisitPlaceList().add(visitplace);
+                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
+                //해당 환자에 visitplace 추가
+                return 1;// 동선 삽입 성공
+            }
+        }
+        return 0;//동선 삽입 실패: 해당 환자 정보가 없음
+    }
+
+
+    /*관리자의 확진자 수정 페이지에서 확진자 동선 정보 하나를 삭제하는 메소드 */
+    public static int AND_delete_pmoving(Patient patient, VisitPlace visitplace) {
+        //patient_info();
+        for (int i = 0; i < patientList.size(); i++) {
+            //1. 환자리스트를 돌며 동일한 환자 찾기.
+            //DB테이블에서의 primary 키값들의 값이 같을 때 동일 환자.
+            Log.d("AND 환자 : ", patient.getPatientNum()+", "+Integer.toString(patient.getSmallLocalNum())+", "+Integer.toString(patient.getBigLocalNum()));
+            if (patient.getSmallLocalNum() == patientList.get(i).getSmallLocalNum() && patient.getBigLocalNum() == patientList.get(i).getBigLocalNum()
+                    && patient.getPatientNum().equals(patientList.get(i).getPatientNum())){
+                //칮으면 해당 환자의 visitplacelist를 돌며 매개변수로 온 visitplace 찾고 삭제하기
+                int t = findIndex(patientList.get(i).getVisitPlaceList(),visitplace);
+                if(t!=-1){
+                    patientList.get(i).getVisitPlaceList().remove(t);
+                }
+                //setPatientList(patientList);
+                Log.d("patient",Integer.toString(patientList.get(i).getVisitPlaceList().size()));
+                return 1;//정상 삭제
+            }
+        }
+        return -1;//환자 정보도 없을 경우
+    }
+    public static int findIndex(ArrayList<VisitPlace> visitplaceList,VisitPlace visitplace2){
+        for(int i =0; i <visitplaceList.size();i++){
+            if(visitplaceList.get(i).getVisitDate().equals(visitplace2.getVisitDate())&&
+                    visitplaceList.get(i).getVisitPlace().get_placeAddress().equals(visitplace2.getVisitPlace().get_placeAddress())){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /*관리자의 확진자 추가, 수정 페이지에서 확진자 동선 정보 하나를 삭제하는 메소드-환자 정보를 삭제하면 관련 동선 정보도 싹다 삭제*/
+    public static void AND_delete_patient(Patient patient) {
+        patientList.remove(patient);
     }
 }
