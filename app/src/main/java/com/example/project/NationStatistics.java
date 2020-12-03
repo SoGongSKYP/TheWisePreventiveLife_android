be34ccab2a77e3aae0f835717070a371d417b411
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-12-03 15:37:48 +0900

    Update NationStatistics.java

--- app/src/main/java/com/example/project/NationStatistics.java
+++ app/src/main/java/com/example/project/NationStatistics.java
@@ -2,29 +2,10 @@ package com.example.project;
 import java.util.*;
 
 /**
- * 양식
- * <item>
- * <accDefRate>1.0684961451</accDefRate> //누적 확진률
- * <accExamCnt>2896746</accExamCnt> //누적 검사수
- * <accExamCompCnt>2845401</accExamCompCnt> // 누적 검사 완료
- * <careCnt>3535</careCnt> //치료중 환자수
- * <clearCnt>26365</clearCnt>//격리해제 수
- * <createDt>2020-11-21 09:35:35.158</createDt> //등록 일시분초
- * <deathCnt>503</deathCnt>//사망자수
- * <decideCnt>30403</decideCnt>//확진자수
- * <examCnt>51345</examCnt>//검사 진행수
- * <resutlNegCnt>2814998</resutlNegCnt>//결과 음성수
- * <seq>329</seq>
- * <stateDt>20201121</stateDt> // 기준 일
- * <stateTime>00:00</stateTime>//기준 시간
- * <updateDt>null</updateDt>
- * </item>
+ * 전국 통계를 담을 사용자 정의형 class
  */
 public class NationStatistics extends Statistics {
-
-    /**
-     * Default constructor
-     */
+    
     public NationStatistics(String staticsDate,int patientNum, int deadNum,int healerNum,
                             int testNum, ArrayList<LocalStatistics> localStatistics,int careNum,
                             int testNeg, int testCnt, int testCntComplete, double accDefRate) {
