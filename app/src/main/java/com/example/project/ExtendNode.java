c11c0602b520254dbab6ac9ec118fdf5c1a54bd2
jimin755 <57628980+jimin755@users.noreply.github.com>
2020-12-03 15:45:59 +0900

    Update ExtendNode.java

--- app/src/main/java/com/example/project/ExtendNode.java
+++ app/src/main/java/com/example/project/ExtendNode.java
@@ -1,9 +1,22 @@
 package com.example.project;
 
-/*
-* 1-9-2요약 정보 확장 노드
+/**
+ * 경로 검색시 사용하는 사용자 정의형 class
+ * 1-9-2요약 정보 확장 노드
+ * 참고: https://lab.odsay.com/guide/sustainReference#searchPubTransPath
 */
 public class ExtendNode {
+    private double trafficDistance; //도보를 제외한 총 이동 거리
+    private int totalWalk; //총 도보 이동 거리
+    private int totalTime; //총 소요시간
+    private int payment; // 총 요금
+    private int busTransitCount; //버스 환승 카운트
+    private int subwayTransitCount; //지하철 환승 카운트
+    private String mapObj; //보간점 API를 호출하기 위한 파라미터 값
+    private String firstStartStation; //최초 출발역/정류장
+    private String lastEndStation; //최종 도착역/정류장
+    private int totalStationCount; //총 정류장 합
+    private double totalDistance; //총 거리
 
     ExtendNode(double trafficDistance,int totalWalk,int totalTime,int payment,int busTransitCount
     ,int subwayTransitCount,String mapObj,String firstStartStation,String lastEndStation,int totalStationCount
@@ -20,17 +33,7 @@ public class ExtendNode {
         this.totalStationCount=totalStationCount;
         this.totalDistance=totalDistance;
     }
-    private double trafficDistance; //도보를 제외한 총 이동 거리
-    private int totalWalk; //총 도보 이동 거리
-    private int totalTime; //총 소요시간
-    private int payment; // 총 요금
-    private int busTransitCount; //버스 환승 카운트
-    private int subwayTransitCount; //지하철 환승 카운트
-    private String mapObj; //보간점 API를 호출하기 위한 파라미터 값
-    private String firstStartStation; //최초 출발역/정류장
-    private String lastEndStation; //최종 도착역/정류장
-    private int totalStationCount; //총 정류장 합
-    private double totalDistance; //총 거리
+
 
     public double getTotalDistance() {
         return totalDistance;
